5ac5c2540b59e2514acac438e91c406dda3a484a
Gurbinder Singh <26441580+gurbindersingh@users.noreply.github.com>
2025-06-30 18:10:02 +0200

    Configure PrimeVue

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,14 @@ import "@tabler/icons-webfont/dist/tabler-icons.min.css";
 import { createApp } from "vue";
 import App from "./view/App.vue";
 import { router } from "./router";
+import PrimeVue from "primevue/config";
+import Button from "primevue/button";
+import Aura from "@primeuix/themes/aura";
 
 const app = createApp(App);
 app.use(router);
+
+app.use(PrimeVue, { theme: { preset: Aura } });
+app.component("PButton", Button);
+
 app.mount("#app");
