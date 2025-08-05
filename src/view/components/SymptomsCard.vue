47dba1fb93bde72f49caa7160e99548b4567ea6a
Gurbinder Singh <26441580+gurbindersingh@users.noreply.github.com>
2025-08-05 20:36:35 +0200

    Finish Symptom Card component

--- src/view/components/SymptomsCard.vue
+++ src/view/components/SymptomsCard.vue
@@ -1,6 +1,13 @@
 <script setup lang="ts">
-import { createHslColorSteps } from "@/services/color-steps";
+import { createColorSteps } from "@/services/color-steps";
 import { computed, ref } from "vue";
+
+const props = defineProps<{
+  symptom: string;
+  severity: number;
+  isEditMode?: boolean;
+  onDelete?: () => void;
+}>();
 
 const severityOptions = [
   {
@@ -26,60 +33,85 @@ const severityOptions = [
       "I had to go to the emergency room to get help for my symptoms.",
   },
 ];
-const colorValues = createHslColorSteps(0, 120, 5).reverse();
+const allSuggestions = ["Headache", "Neck pain", "Backpain", "Brainfog"];
+const colorValues = createColorSteps(77, 142, 5).reverse();
+// const colorValues = createColorSteps(0, 120, 5).reverse();
 
-const isEditMode = ref(true);
-const symptom = ref("Symptoms");
-const selectedSeverity = ref(severityOptions[1]);
+const isEditMode = ref(props.isEditMode);
+const symptom = ref(props.symptom ? props.symptom : "");
+const selectedSeverity = ref(severityOptions[props.severity]);
 
-const filteredSuggestions = computed(() => ["foo", "bar"]);
-const symptomIsEmpty = computed(() => false);
+const filteredSuggestions = ref([] as string[]);
+const symptomIsEmpty = computed(() => symptom.value.trim().length < 1);
 
-function search() {}
+function search(event: { query: string }) {
+  filteredSuggestions.value = allSuggestions.filter((s) =>
+    s.toLowerCase().includes(event.query.toLowerCase()),
+  );
+}
 </script>
 
 <template>
   <PCard class="symptoms-card">
     <template #content>
-      <div class="is-flex mb-4 is-align-items-center">
-        <PAutoComplete
-          class="is-flex-grow-1"
-          v-model="symptom"
-          :emptySearchMessage="'Adding: ' + symptom"
-          :suggestions="filteredSuggestions"
-          :invalid="symptomIsEmpty"
-          completeOnFocus
+      <div class="edit-mode" v-if="isEditMode">
+        <div class="is-flex mb-2 is-align-items-center">
+          <PAutoComplete
+            class="is-flex-grow-1"
+            v-model="symptom"
+            :emptySearchMessage="'Add new symptom: ' + symptom"
+            :suggestions="filteredSuggestions"
+            :invalid="symptomIsEmpty"
+            completeOnFocus
+            fluid
+            @complete="search"
+          />
+          <PButton icon="ti ti-trash" variant="text" severity="contrast" />
+        </div>
+        <PSelectButton
+          class="severity-selector mb-2"
+          v-model="selectedSeverity"
+          :options="severityOptions"
+          optionLabel="value"
+          size="large"
           fluid
-          @complete="search"
         />
-        <PButton icon="ti ti-trash" variant="text" severity="contrast" />
+        <p
+          class="description mb-2"
+          :style="{
+            color: colorValues[selectedSeverity.value - 1].css,
+          }"
+        >
+          <span>Explanation:</span>
+          <span class="ml-1" v-html="selectedSeverity.description"></span>
+        </p>
+        <PButton
+          label="Save"
+          severity="secondary"
+          fluid
+          rounded
+          :disabled="symptomIsEmpty"
+          :onClick="() => (isEditMode = false)"
+        />
       </div>
-      <p
-        class="description mb-2"
-        :style="{
-          color: `hsl(${colorValues[selectedSeverity.value - 1]} 50 50)`,
-        }"
-      >
-        <span>Explanation:</span>
-        <span class="ml-1" v-html="selectedSeverity.description"></span>
-      </p>
-      <PSelectButton
-        class="severity-selector mb-4"
-        v-model="selectedSeverity"
-        :options="severityOptions"
-        optionLabel="value"
-        size="large"
-        fluid
-      />
-      <PButton
-        label="Save"
-        severity="contrast"
-        variant="outlined"
-        fluid
-        rounded
-        :disabled="symptomIsEmpty"
-        :onClick="() => (isEditMode = false)"
-      />
+      <div class="show-mode is-flex is-align-items-center" v-else>
+        <p class="activity title has-text-weight-medium is-size-6 m-0">
+          {{ symptom }}
+        </p>
+        <PButton
+          variant="text"
+          icon="ti ti-settings"
+          severity="secondary"
+          size="large"
+          :onClick="() => (isEditMode = true)"
+        />
+        <div class="spacer is-flex-grow-1"></div>
+        <p class="description has-text-grey">
+          <span class="is-capitalized mr-1"
+            >Severity: {{ selectedSeverity.value }}/5</span
+          >
+        </p>
+      </div>
     </template>
   </PCard>
 </template>
