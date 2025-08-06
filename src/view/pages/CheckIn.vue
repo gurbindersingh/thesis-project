ae4b687dc3c6bbfc45da4a7b82d776f638670bc5
Gurbinder Singh <26441580+gurbindersingh@users.noreply.github.com>
2025-08-06 20:50:13 +0200

    Refactor

--- src/view/pages/CheckIn.vue
+++ src/view/pages/CheckIn.vue
@@ -1,47 +1,10 @@
 <script setup lang="ts">
-import YesterdayCheckIn from "../components/YesterdayCheckIn.vue";
+import TodayCheckIn from "../components/TodayCheckIn.vue";
 </script>
 
 <template>
   <div id="check-in">
-    <h1 class="title is-size-4">Daily Check-In</h1>
-    <YesterdayCheckIn />
-    <!--   <div class="crash"> -->
-    <!--     <h2 class="title is-6">Are you having a crash?</h2> -->
-    <!--     <PCard> -->
-    <!--       <template #content> -->
-    <!--         <p>Test</p> -->
-    <!--       </template> -->
-    <!--     </PCard> -->
-    <!--   </div> -->
-    <!---->
-    <!--   <div class="sleep"> -->
-    <!--     <h2>How did you sleep?</h2> -->
-    <!-- Fetch length from Health API -->
-    <!--     <PCard> -->
-    <!--       <template #content> -->
-    <!--         <p>Test</p> -->
-    <!--       </template> -->
-    <!--     </PCard> -->
-    <!--   </div> -->
-    <!---->
-    <!--   <div class="activities"> -->
-    <!--     <h2>Activities</h2> -->
-    <!--     <PCard> -->
-    <!--       <template #content> -->
-    <!--         <p>Test</p> -->
-    <!--       </template> -->
-    <!--     </PCard> -->
-    <!--   </div> -->
-    <!---->
-    <!--   <div class="hrv"> -->
-    <!--     <h2>Heart Rate Variability</h2> -->
-    <!--     <PButton label="Measure HRV >" /> -->
-    <!--   </div> -->
-    <!---->
-    <!--   <div class="grip-strength"> -->
-    <!--     <h2>Grip Strength</h2> -->
-    <!--     <PButton label="Measure grip strength >" /> -->
-    <!--   </div> -->
+    <h1 class="title is-size-4 has-text-grey-light">Daily Check-In</h1>
+    <TodayCheckIn />
   </div>
 </template>
