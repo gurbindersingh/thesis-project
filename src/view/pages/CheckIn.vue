3203c4956a9e6f0262c77ba5c3139095406f8d4b
Gurbinder Singh <26441580+gurbindersingh@users.noreply.github.com>
2025-10-12 19:48:56 +0200

    Implement View Options

--- src/view/pages/CheckIn.vue
+++ src/view/pages/CheckIn.vue
@@ -1,10 +1,47 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import TodayCheckIn from "../components/TodayCheckIn.vue";
+
+const menu = ref();
+
+const toggle = (event: Event) => {
+  menu.value.toggle(event);
+};
 </script>
 
 <template>
   <div id="check-in">
-    <h1 class="title is-size-4 has-text-primary">Daily Check-In</h1>
+    <div>
+      <h1 class="title is-size-4 has-text-primary">
+        Daily Check-In
+        <PButton
+          class=""
+          icon="ti ti-settings"
+          iconPos="right"
+          size="large"
+          variant="text"
+          rounded
+          fluid
+          :onClick="toggle"
+          aria-haspopup="true"
+          aria-controls="overlay_menu"
+        />
+      </h1>
+      <PMenu
+        ref="menu"
+        id="overlay_menu"
+        :model="[
+          {
+            label: 'View Options',
+            items: [
+              { label: 'Everything on Single Page', icon: 'ti ti-file' },
+              { label: 'One Page per Section', icon: 'ti ti-files' },
+            ],
+          },
+        ]"
+        :popup="true"
+      />
+    </div>
     <TodayCheckIn />
   </div>
 </template>
